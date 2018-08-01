77d53b3bef97c196eb93e2780936fc404a6c8e6b
Warren Bailey <warren@warrenbailey.net>
2018-08-01 10:55:58 +0100

    Updating Iotas

--- iota.go
+++ iota.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
+const (
+	Apple int = iota
+	Orange
+	Banana
+)
+
 func main() {
-	$END$
+	fmt.Printf("The value of Apple is %v\n", Apple)
+	fmt.Printf("The value of Orange is %v\n", Orange)
+	fmt.Printf("The value of Banana is %v\n", Banana)
 }
--- loglevels.go
+++ loglevels.go
@@ -1,5 +1,50 @@
 package main
 
+import "fmt"
+
+const (
+	Warn int = iota
+	Debug
+	Info
+	Error
+	Fatal
+)
+
 func main() {
-	$END$
+	setLevel(Warn)
+	setLevel(Debug)
+	setLevel(Info)
+	setLevel(Error)
+	setLevel(Fatal)
+	setLevel(7)
+}
+
+func setLevel(level int) {
+	switch level {
+	case Warn:
+		{
+			fmt.Printf("Warn Level %d\n", level)
+		}
+	case Debug:
+		{
+			fmt.Printf("Debug level %d\n", level)
+		}
+	case Info:
+		{
+			fmt.Printf("Info level %d\n", level)
+		}
+	case Error:
+		{
+			fmt.Printf("Error level %d\n", level)
+		}
+	case Fatal:
+		{
+			fmt.Printf("Fatal level %d\n", level)
+		}
+	default:
+		{
+			fmt.Printf("Level %v\n", level)
+		}
+
+	}
 }
